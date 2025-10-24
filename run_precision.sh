7aef2889f90a32b9c2141fd4106abfd6f04d4445
romaingrvl <gervalle.phys@gmail.com>
2025-10-24 16:07:51 +0100

    Generated the reference solution for the first DBS

--- run_precision.sh
+++ run_precision.sh
@@ -4,20 +4,20 @@ SOURCEFILE=mpi_spin_bs.edp
 
 c=12.635047045
 m=0
-om=0.9
+om=0.99
 odd=1
-nx=130
+nx=647
 ny=50
 
-n=20
+n=10
 
 mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=0;i<$n;i++))
 do
-	nx=$(bc -l <<<"$nx+1")
+	ny=$(bc -l <<<"$ny+5")
 	
-	echo "Nb. points = $nx"
+	echo "Nb. points = $ny"
 	
 	mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 	
