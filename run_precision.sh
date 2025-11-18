613a4ac27f1c1989e781a70ffab604014db80d00
romaingrvl <gervalle.phys@gmail.com>
2025-11-18 11:00:25 +0000

    Try to improve the third DBS solution

--- run_precision.sh
+++ run_precision.sh
@@ -6,16 +6,16 @@ c=1
 m=0
 om=0.75
 odd=1
-nx=130
+nx=680
 ny=50
 
-n=20
+n=10
 
 mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=0;i<$n;i++))
 do
-	nx=$(bc -l <<<"$nx+50")
+	nx=$(bc -l <<<"$nx+1")
 	#ny=$(bc -l <<<"$ny+10")
 	
 	echo "Nb. points = $ny"
