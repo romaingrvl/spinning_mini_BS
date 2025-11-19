6e84d06f331dd63ce971ed1bd589ac463aa1cd9c
romaingrvl <gervalle.phys@gmail.com>
2025-11-19 10:28:24 +0000

    Constructed the third DBS solution

--- run_precision.sh
+++ run_precision.sh
@@ -6,20 +6,20 @@ c=1
 m=0
 om=0.75
 odd=1
-nx=680
-ny=50
+nx=800
+ny=230
 
 n=10
 
-mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
+mpirun -np 6 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=0;i<$n;i++))
 do
-	nx=$(bc -l <<<"$nx+1")
-	#ny=$(bc -l <<<"$ny+10")
+	#nx=$(bc -l <<<"$nx+1")
+	ny=$(bc -l <<<"$ny+1")
 	
 	echo "Nb. points = $ny"
 	
-	mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
+	mpirun -np 6 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 	
 done
