45c5dc0cee57bf4d7c4317c3d8aa27e5f5b02d4d
romaingrvl <gervalle.phys@gmail.com>
2025-11-21 11:06:20 +0000

    Computed the typical computation time and started convergency test

--- run_precision.sh
+++ run_precision.sh
@@ -2,24 +2,24 @@
 
 SOURCEFILE=mpi_spin_bs.edp
 
-c=1
-m=0
-om=0.75
-odd=1
-nx=800
-ny=230
+c=1.835
+m=1
+om=0.8
+odd=0
+nx=19
+ny=100
 
-n=10
+n=281
 
-mpirun -np 6 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
+mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -acu 15 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=0;i<$n;i++))
 do
-	#nx=$(bc -l <<<"$nx+1")
-	ny=$(bc -l <<<"$ny+1")
+	nx=$(bc -l <<<"$nx+1")
+	#ny=$(bc -l <<<"$ny+1")
 	
 	echo "Nb. points = $ny"
 	
-	mpirun -np 6 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
+	mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -acu 15 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 	
 done
