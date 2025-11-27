fee337c9488b2e0eb3cd43a663c53607ba4d2c66
romaingrvl <gervalle.phys@gmail.com>
2025-11-27 14:43:16 +0000

    Tried to add more points for the convergency test

--- run_precision.sh
+++ run_precision.sh
@@ -6,16 +6,16 @@ c=1.835
 m=1
 om=0.8
 odd=0
-nx=216
-ny=100
+nx=300
+ny=50
 
-n=281
+n=35
 
 mpirun -np 6 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -acu 15 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=0;i<$n;i++))
 do
-	nx=$(bc -l <<<"$nx+1")
+	nx=$(bc -l <<<"$nx+20")
 	#ny=$(bc -l <<<"$ny+1")
 	
 	echo "Nb. points = $ny"
