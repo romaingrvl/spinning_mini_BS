693ae0d6636e1b399f247ed46202b8b3cd085510
romaingrvl <gervalle.phys@gmail.com>
2025-12-04 18:32:57 +0000

    Reconstructed the full 2nd branch of solutions for RBS

--- run_c.sh
+++ run_c.sh
@@ -5,15 +5,15 @@ LOGFILE=log.txt
 
 bc=0
 m=1
-om=0.995
+om=0.922773
 odd=0
-c1=3.19062042
-c2=7.0
+c1=0.7614
+c2=1.0
 
 nx=137
 ny=54
 n=20
-inc=1
+inc=-1
 
 if (( $inc != -1 ))
 then
@@ -24,7 +24,7 @@ fi
 
 stp=$(bc -l <<<"scale=9;($c2-$c1)/($n-1)")
 
-mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -load 1 -save 1 -quant 1 -2Dplot 0 -bc $bc -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
+#mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -load 1 -save 1 -quant 1 -2Dplot 0 -acu 15 -bc $bc -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=1;i<$n;i++))
 do
@@ -37,7 +37,7 @@ do
 	fi
 	
 	echo "c = $c"
-	mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -load 1 -save 1 -quant 1 -2Dplot 0 -bc $bc -cload $c0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny | tee $LOGFILE
+	mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -load 1 -save 1 -quant 1 -2Dplot 0 -acu 15 -bc $bc -cload $c0 -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny | tee $LOGFILE
 	
 	CHECK_CONV=$(cat $LOGFILE | grep "Convergence=1")
 	
@@ -47,8 +47,8 @@ do
 		c=$c0
 		stp=$(bc -l <<<"scale=9;$stp*0.5")
 	else
-		stp=$(bc -l <<<"scale=9;$stp*1.01")
-		#stp=$(bc -l <<<"scale=9;$stp*1.0")
+		#stp=$(bc -l <<<"scale=9;$stp*1.01")
+		stp=$(bc -l <<<"scale=9;$stp*1.0")
 	fi
 	
 done
--- run_om.sh
+++ run_om.sh
@@ -4,23 +4,23 @@ SOURCEFILE=mpi_spin_bs.edp
 LOGFILE=log.txt
 
 bc=0
-c=8.0
+c=0.7614
 m=1.0
-om=0.99999755
+om=0.9
 odd=0
 nx=137
 ny=54
 
-del=0.000000488
-n=30
+del=0.0005
+n=80
 
 #mpirun -np 4 FreeFem++-mpi -ng $SOURCEFILE -save 1 -load 1 -quant 1 -2Dplot 0 -acu 15 -bc $bc -c $c -m $m -om $om -odd $odd -nx $nx -ny $ny
 
 for ((i=0;i<$n;i++))
 do
 	om0=$om
-	#om=$(bc -l <<<"scale=12;$om-$del")
-	om=$(bc -l <<<"scale=12;$om+$del")
+	om=$(bc -l <<<"scale=12;$om-$del")
+	#om=$(bc -l <<<"scale=12;$om+$del")
 	
 	echo "Omega = $om"
 	
@@ -33,6 +33,8 @@ do
 		echo "NOT CONVERGED"
 		om=$om0
 		del=$(bc -l <<<"scale=12;$del*0.5")
+	else
+		del=$(bc -l <<<"scale=12;$del*1.01")
 	fi
 	
 done
